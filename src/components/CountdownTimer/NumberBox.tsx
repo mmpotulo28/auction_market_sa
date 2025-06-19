70e53238bcca787beaf13e78c80f2025a616ebfe
Manelisi Mpotulo <mpotulom28@gmail.com>
2025-06-19 15:53:58 +0200

    feat: update mobile styles for HowItWorks component to enhance layout and video display

--- src/components/CountdownTimer/NumberBox.tsx
+++ src/components/CountdownTimer/NumberBox.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./countdown-timer.module.css";
 
 interface numProp {
 	num: string | number;
@@ -7,65 +8,13 @@ interface numProp {
 
 export const NumberBox = ({ num, unit }: numProp) => {
 	return (
-		<div
-			style={{
-				display: "flex",
-				flexDirection: "column",
-				alignItems: "center",
-				marginTop: "1rem",
-				paddingLeft: 0,
-				paddingRight: 0,
-			}}>
-			<div
-				style={{
-					position: "relative",
-					background: "transparent",
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-					justifyContent: "center",
-					borderRadius: "0.5rem",
-					width: "3rem",
-					height: "3rem",
-					marginTop: "1rem",
-				}}>
-				<div
-					style={{
-						borderTopLeftRadius: "0.5rem",
-						borderTopRightRadius: "0.5rem",
-						background: "#343650",
-						width: "100%",
-						height: "100%",
-					}}></div>
-				<div
-					style={{
-						color: "var(--color-foreground)",
-						fontSize: "1.75rem",
-						position: "absolute",
-						zIndex: 10,
-						fontWeight: "bold",
-						fontFamily: "monospace",
-					}}>
-					{num}
-				</div>
-				<div
-					style={{
-						borderBottomLeftRadius: "0.5rem",
-						borderBottomRightRadius: "0.5rem",
-						background: "#2c2e3f",
-						width: "100%",
-						height: "100%",
-					}}></div>
+		<div className={styles.numberBox}>
+			<div className={styles.numberBoxInner}>
+				<div className={styles.numberBoxTop}></div>
+				<div className={styles.numberBoxNum}>{num}</div>
+				<div className={styles.numberBoxBottom}></div>
 			</div>
-			<p
-				style={{
-					color: "var(--color-foreground)",
-					fontSize: "1.125rem",
-					marginTop: "0.75rem",
-					fontWeight: 600,
-				}}>
-				{unit}
-			</p>
+			<p className={styles.numberBoxUnit}>{unit}</p>
 		</div>
 	);
 };
