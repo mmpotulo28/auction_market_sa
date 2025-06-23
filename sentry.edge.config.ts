a9c18933e5df06b677de1d75d5bf170f288babac
Manelisi Mpotulo <mpotulom28@gmail.com>
2025-06-23 14:31:49 +0200

    feat: integrate Sentry for error tracking and monitoring
    - Updated package.json to remove turbopack from dev script and added @sentry/nextjs dependency.
    - Created sentry.edge.config.ts for Sentry initialization in edge functions.
    - Created sentry.server.config.ts for Sentry initialization on the server.
    - Added a faulty API route (sentry-example-api) to test Sentry's error monitoring.
    - Implemented global error handling component (global-error.tsx) to capture exceptions with Sentry.
    - Developed a sample page (sentry-example-page) to demonstrate Sentry's frontend error tracking.
    - Configured client-side instrumentation in instrumentation-client.ts for Sentry.
    - Added instrumentation logic in instrumentation.ts to register Sentry configurations based on runtime.

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7a5c4086f4d721a3e332351fd1c46d2a@o4509547924553728.ingest.us.sentry.io/4509547928092672",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7a5c4086f4d721a3e332351fd1c46d2a@o4509547924553728.ingest.us.sentry.io/4509547928092672",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
